7fac746bd82ad49401694afa94bc4300be823db7
dzinh1901-lang <dzinh1901@gmail.com>
2026-03-31 22:07:37 +0900

    feat(api): requirement module

---
+++ apps/api/src/modules/requirement/requirement.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { RequirementController } from './requirement.controller';
+import { RequirementService } from './requirement.service';
+
+@Module({
+  controllers: [RequirementController],
+  providers: [RequirementService],
+})
+export class RequirementModule {}
